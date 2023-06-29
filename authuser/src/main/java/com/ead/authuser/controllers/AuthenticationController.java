4acfe3e9238c72c31279ac03fb0d3cf0062abd7e
Mateus Maciel Nere <mateus.macieln@gmail.com>
2023-06-29 14:37:16 -0300

    Finishing CRUD for user.

---
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -0,0 +1,53 @@
+package com.ead.authuser.controllers;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ead.authuser.dtos.UserDTO;
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserService;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 36000)
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser(@RequestBody UserDTO userDTO) {
+
+        if(userService.existsByUsername(userDTO.getUsername())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username already taken!");
+        }
+
+        if(userService.existsByEmail(userDTO.getEmail())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email already taken!");
+        }
+
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDTO, userModel);
+        userModel.setUserStatus(UserStatus.ACTIVE);
+        userModel.setUserType(UserType.STUDENT);
+        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        userService.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+    
+}
